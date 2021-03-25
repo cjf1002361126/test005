a069abb16b16579d5052d1f8d96a778660ce959f
zssise <1002361126@qq.com>
2021-03-25 22:21:32 +0800

    1002361修改了

--- ImageFullGc.java
+++ ImageFullGc.java
@@ -7,8 +7,10 @@ import javax.swing.ImageIcon;
 public class ImageFullGc {
 
 	public static Image getImage(String path) { 
-	    ImageIcon icon = new ImageIcon(path); 
 	    Image img = icon.getImage(); 
+	    ImageIcon icon = new ImageIcon(path); 
+	    // Image image = getImage("C:\\Users\\cjf\\Desktop\\BaiduShurufa_2020-3-8_19-3-13.png");
+        // image.flush();
 	    return img; 
 	}
 	
